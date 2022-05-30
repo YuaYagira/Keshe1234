d66cefa102daa52575fe254643ecd25cabb86836
Yuay <Yuay@DELL-INSPIRON>
2022-05-30 19:29:57 +0800

    添加项目文件。

---
+++ 课设/Player.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "common.h"
+class Player
+{
+public:
+	Player(void) {};
+	~Player(void) {};
+	int  level, HP, ATK, MP;
+	string ID;
+	string gender;
+};
---
+++ 课设/User.h
@@ -0,0 +1,10 @@
+#pragma once
+#include"common.h"
+using namespace std;
+class User {
+public:
+    User() {}
+    ~User() {}
+    string userName;
+    string password;
+};
